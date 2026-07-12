72b69e54e69489bd69a181a0cf5bf5892afedd45
ShreyasBile <159661417+ShreyasBile@users.noreply.github.com>
2026-07-13 02:24:22 +0530

    Time: 1048 ms (44.41%), Space: 7.6 MB (99.66%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int x=0;
+        for(int i=1; i<=num/2; i++){
+            if(num%i==0){
+                x=x+i;
+            }
+        }
+        return x==num;
+    }
+};
