7494a4e6d4bf69f8eb6ff7aa7dadd8310bcb1146
ShreyasBile <159661417+ShreyasBile@users.noreply.github.com>
2026-07-13 02:16:46 +0530

    Time: 0 ms (100%), Space: 8 MB (12.11%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        int count=0;
+        while(n>0){
+            if(n%2==1){
+                count++;
+            }
+            n=n/2;
+        }
+        return count==1;
+    }
+};
